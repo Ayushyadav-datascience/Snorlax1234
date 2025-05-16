00dea38b2878f172218b62c1a0d59042b11aedb0
Ayushyadav-datascience <ayushyadav.datascience@gmail.com>
2025-05-16 23:37:28 +0530

    donr

---
+++ Ayush.py
@@ -0,0 +1,5 @@
+def Ayush(a,b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error"
---
+++ Main.py
@@ -0,0 +1,36 @@
+import Ayush
+import Tanmay
+import Aditya
+import Samridhi
+
+
+
+
+
+def calc(a, b, op):
+    if op == '+':
+        Aditya(a,b)
+        
+    elif op == '-':
+        Tanmay(a,b)
+        
+    elif op == '*':
+        Samridhi(a,b)
+        
+    elif op == '/':
+        Ayush(a,b)
+        
+    else:
+        return "Invalid operation"
+
+
+try:
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number: "))
+    op = input("Enter the operation (+, -, *, /): ")
+
+    result = calc(a, b, op)
+    print("Result:", result)
+
+except ValueError:
+    print("Invalid input. Please enter numeric values.")
